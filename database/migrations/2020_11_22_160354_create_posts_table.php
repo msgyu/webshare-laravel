f4fd8bb85c78c9e5a426a730854ec86c1f717c71
nousi <nousidayo@gmail.com>
2020-11-22 16:12:30 +0900

    Add onDelete in posts migrate

--- database/migrations/2020_11_22_160354_create_posts_table.php
+++ database/migrations/2020_11_22_160354_create_posts_table.php
@@ -18,7 +18,7 @@ class CreatePostsTable extends Migration
             $table->string('title');
             $table->string('body');
             $table->unsignedBigInteger('user_id');
-            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');;
             $table->timestamps();
         });
     }
