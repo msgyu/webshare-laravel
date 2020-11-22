4a84a6845f8b0ec91af4e5555c4e1383f7bdf749
nousi <nousidayo@gmail.com>
2020-11-22 18:35:05 +0900

    Create lp-header.blade.php

---
+++ resources/views/layouts/lp-header.blade.php
@@ -0,0 +1,12 @@
+<div class="header clearfix">
+  <a class="logo" href="/"><img class="logo-image" src="/assets/sample/webshare-icon-bed26559e46c4ffdef0739881c1a92a28fce035eaa0b6ab8ea248c252bd444ab.png">
+    <ul class="logo-ul">
+      <li class="logo-ul-li_sub">
+        <p class="logo-subtitle">影響力をシェアしよう</p>
+      </li>
+      <li class="logo-ul-li">
+        <h1 class="logo-h1">WEBSHARE</h1>
+      </li>
+    </ul>
+  </a>
+</div>
