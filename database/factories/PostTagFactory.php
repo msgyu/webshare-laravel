904ceffe3693ccc23ec574662cabb5160c7160e9
nousi <nousidayo@gmail.com>
2020-11-22 16:06:24 +0900

    RUN "php artisan make:model post_tag -a"

---
+++ database/factories/PostTagFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\post_tag;
+use Faker\Generator as Faker;
+
+$factory->define(post_tag::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
