2e4d715a399da846a303939d63463899d6092022
nousi <nousidayo@gmail.com>
2020-11-23 22:24:34 +0900

    ロゴ画像を挿入

--- resources/views/layouts/lp-header.blade.php
+++ resources/views/layouts/lp-header.blade.php
@@ -1,5 +1,5 @@
 <div class="header clearfix">
-  <a class="logo" href="/"><img class="logo-image" src="/assets/sample/webshare-icon-bed26559e46c4ffdef0739881c1a92a28fce035eaa0b6ab8ea248c252bd444ab.png">
+  <a class="logo" href="/"><img class="logo-image" src="{{ asset('./img/sample/webshare-icon.png') }}">
     <ul class="logo-ul">
       <li class="logo-ul-li_sub">
         <p class="logo-subtitle">影響力をシェアしよう</p>
