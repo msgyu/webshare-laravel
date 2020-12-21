b86bdf66518097e67b2e7ccf8b6b51f6b9c604ba
nousi <nousidayo@gmail.com>
2020-12-21 23:00:24 +0900

    Create main.blade.php
    posts.indexの投稿一覧を表示するHTML

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,15 @@
+<div class="row main">
+  <h2 class="main-h2">コラボ相手を探す</h2>
+  <p class="main_search-text">
+    0人のクリエイターがみつかりました。クリエイターがみつからない時は募集してみよう！
+  </p>
+  <div class="jscroll">
+    <div class="jscroll-inner">
+
+      <div class="skill-list">
+
+
+      </div>
+    </div>
+  </div>
+</div>
