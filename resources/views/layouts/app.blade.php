30591c6c9a7719eb30d90a3d797d9d2b9dac94a8
nousi <nousidayo@gmail.com>
2020-12-23 19:46:04 +0900

    [Add] fontawesomeを読み込むscriptを追加

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
 
     <!-- Scripts -->
     <script src="{{ mix('js/app.js') }}" defer></script>
+    <script src="https://kit.fontawesome.com/ffe6531265.js" crossorigin="anonymous"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
