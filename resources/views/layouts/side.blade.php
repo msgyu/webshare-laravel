d3b51148c0415256a5b3143f8b7dc2729924a345
nousi <nousidayo@gmail.com>
2020-12-23 16:35:31 +0900

    search-sideの子要素をform内に移行

--- resources/views/layouts/side.blade.php
+++ resources/views/layouts/side.blade.php
@@ -1,60 +1,62 @@
 <div class="row side">
   <div class="search-side">
-    <div class="search-side_child">
-      <p>条件で絞り込む</p>
-      <div class="search_wap">
-        <form class="user_search" id="user_search" action="/users/search" accept-charset="UTF-8" method="get"><input name="utf8" type="hidden" value="✓"><input placeholder="何かお探しですか？" class="input" type="text" name="q[nickname_or_title_or_profile_or_cando_cont]" id="q_nickname_or_title_or_profile_or_cando_cont">
-          <button name="button" type="submit" class="search__button">
-            <div class="icon_wap">
-              <i class="fas fa-search" aria-hidden="true"></i>
-            </div>
-          </button></form>
+    <form action="">
+      <div class="search-side_child">
+        <p>条件で絞り込む</p>
+        <div class="search_wap">
+          <form class="user_search" id="user_search" action="/users/search" accept-charset="UTF-8" method="get"><input name="utf8" type="hidden" value="✓"><input placeholder="何かお探しですか？" class="input" type="text" name="q[nickname_or_title_or_profile_or_cando_cont]" id="q_nickname_or_title_or_profile_or_cando_cont">
+            <button name="button" type="submit" class="search__button">
+              <div class="icon_wap">
+                <i class="fas fa-search" aria-hidden="true"></i>
+              </div>
+            </button></form>
+        </div>
       </div>
-    </div>
-    <div class="search-side_child">
-      <p>並び順</p>
-      <div class="select_wrap">
+      <div class="search-side_child">
+        <p>並び順</p>
+        <div class="select_wrap">
+          <select class="search-side_child_select form-control">
+            <option></option>
+            <option>実績順</option>
+            <option>新着順</option>
+            <option>フォロワー多い順</option>
+            <option>フォロワー少ない順</option>
+          </select>
+        </div>
+      </div>
+      <div class="search-side_child">
+        <p>カテゴリ</p>
         <select class="search-side_child_select form-control">
           <option></option>
-          <option>実績順</option>
-          <option>新着順</option>
-          <option>フォロワー多い順</option>
-          <option>フォロワー少ない順</option>
+          <option>a</option>
+          <option>b</option>
         </select>
       </div>
-    </div>
-    <div class="search-side_child">
-      <p>カテゴリ</p>
-      <select class="search-side_child_select form-control">
-        <option></option>
-        <option>a</option>
-        <option>b</option>
-      </select>
-    </div>
-    <div class="search-side_child">
-      <p>料金</p>
-      <select class="search-side_child_select form-control">
-        <option></option>
-        <option>~3000円</option>
-        <option>~5000円</option>
-        <option>~8000円</option>
-        <option>~10000円</option>
-        <option>~12000円</option>
-        <option>~12000円</option>
-        <option>~30000円</option>
-      </select>
-    </div>
-    <div class="search-side_child">
-      <p>評価</p>
-      <select class="search-side_child_select form-control">
-        <option></option>
-        <option>★</option>
-        <option>★★</option>
-        <option>★★★</option>
-        <option>★★★★</option>
-        <option>★★★★★</option>
-      </select>
-    </div>
+      <div class="search-side_child">
+        <p>料金</p>
+        <select class="search-side_child_select form-control">
+          <option></option>
+          <option>~3000円</option>
+          <option>~5000円</option>
+          <option>~8000円</option>
+          <option>~10000円</option>
+          <option>~12000円</option>
+          <option>~12000円</option>
+          <option>~30000円</option>
+        </select>
+      </div>
+      <div class="search-side_child">
+        <p>評価</p>
+        <select class="search-side_child_select form-control">
+          <option></option>
+          <option>★</option>
+          <option>★★</option>
+          <option>★★★</option>
+          <option>★★★★</option>
+          <option>★★★★★</option>
+        </select>
+      </div>
+    </form>
   </div>
 
 </div>
